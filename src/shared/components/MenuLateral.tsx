311069c1c7362cba1b93d5ad2c962502eb3f9109
Chilengue <chilenguejoseorlando@gmail.com>
2024-10-06 03:36:10 +0200

    menu lateral

--- src/shared/components/MenuLateral.tsx
+++ src/shared/components/MenuLateral.tsx
@@ -1,19 +1,60 @@
-import { Box, Drawer, useTheme } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
 
-interface IMenuLateral{
-    children:React.ReactNode;
+interface IMenuLateral {
+  children: React.ReactNode;
 }
 
-export const MenuLateral: React.FC<IMenuLateral> = ({children}) => {
-    const theme=useTheme();
+export const MenuLateral: React.FC<IMenuLateral> = ({ children }) => {
+  const theme = useTheme();
   return (
-   <>
-    <Drawer variant="permanent">
-      Tetse
-    </Drawer>
-    <Box  height="100vh" marginLeft={theme.spacing(28)}>
-        {children}
+    <>
+      <Drawer variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          display="flex"
+          height="100%"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="center"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>Home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItemButton>
+            </List>
+          </Box>
         </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
     </>
   );
 };
